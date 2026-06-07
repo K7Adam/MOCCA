72fe2d5d953d11b06ad2fb0d41939ae9c5693fa4
Adam Karepin <77991070+K7Adam@users.noreply.github.com>
2026-06-07 14:36:27 +0200

    chore: remove unused fillMaxSize import from MeshGradientHeroSurface

--- composeApp/src/commonMain/kotlin/com/mocca/app/ui/components/modern/MeshGradientHeroSurface.kt
+++ composeApp/src/commonMain/kotlin/com/mocca/app/ui/components/modern/MeshGradientHeroSurface.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
